3b22dfb9827637d1d642860cd444153ab2a4e181
abdirahmanabdullahiissa <abdirahman.abdullahi@student.moringaschool.com>
2024-01-09 19:41:08 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,72 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+#!/usr/bin/env python3
+
+APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua",
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer",
+]
+#!/usr/bin/env python3
+
+
 class Dog:
-    pass
+    def __init__(self, name="london", breed="Pug"):
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    @property
+    def name(self):
+        print(f" accessed {self._name}")
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        print(value)
+        if isinstance(value, str) and (1 <= len(value) <= 25):
+            print(f"{self._name} is now {value}")
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed_val):
+        if isinstance(breed_val, str) and (1 <= len(breed_val) <= 25):
+            print(f"{breed_val._breed} is now {breed_val}")
+            self._breed = breed_val
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+
+fido = Dog("Carla")  # Corrected: Pass a strinyyyg as the name argument
+print(fido.name)
+fido.name = "kkkkk"
+print(fido.__dict__)
+
+        
+
+
+
+    
+   
+
+    
+         
+     
--- lib/person.py
+++ lib/person.py
@@ -14,6 +14,52 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
+#!/usr/bin/env python3
+
+APPROVED_JOBS = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing",
+]
+
 
 class Person:
-    pass
+    def __init__(self, name="Sonia", job="Admin"):
+        self._name = ""
+        self._job = ""
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if type(value) == str and (1 <= len(value) <= 25):
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job_value):
+        if job_value in APPROVED_JOBS:
+            self._job = job_value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+
+guido = Person("Hala", "ITC")
+print(type(guido))
